47e007d8b056f2a3de83e45052c9f54ad4ad7ed4
AndrewParker770 <2389622P@student.gla.ac.uk>
2021-11-03 16:02:12 +0000

    Updated look of HTML pages

--- django_videography_project/videography/views.py
+++ django_videography_project/videography/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context_dict = {'boldmessage': 'Cookies and Cream!'}
+    context_dict = {'currentpage': 'index'}
     return render(request, 'videography/index.html', context=context_dict)
 
 
 def about(request):
-    context_dict = {'boldmessage': 'One small step for man!'}
+    context_dict = {'currentpage': 'about'}
     return render(request, 'videography/about.html', context=context_dict)
 
 def video(request):
-    context_dict = {'boldmessage': 'Welcome to the video page!'}
+    context_dict = {'currentpage': 'video'}
     return render(request, 'videography/video.html', context=context_dict)

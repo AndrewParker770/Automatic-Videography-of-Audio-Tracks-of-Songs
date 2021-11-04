dc67f5862527bc40a1ee22cb1c7868998f9a602a
AndrewParker770 <2389622P@student.gla.ac.uk>
2021-11-04 01:31:06 +0000

    Added lyric retrivial program

---
+++ Source/lyrics.py
@@ -0,0 +1,14 @@
+from lyricsgenius import Genius
+import sys
+
+ACCESS_TOKEN = "aqMxsR0rc6Uz9WCDm0WPyqRBuHrSSgyuEVYDadc75f7bkeke83GBJRZaFyN7_W5T"
+
+if __name__ == "__main__":
+    artist_name = sys.argv[1]
+
+    genius = Genius(ACCESS_TOKEN)
+    artist = genius.search_artist(artist_name, max_songs=3, sort="title")
+
+    song = artist.song("Raar")
+
+    print(song.lyrics)

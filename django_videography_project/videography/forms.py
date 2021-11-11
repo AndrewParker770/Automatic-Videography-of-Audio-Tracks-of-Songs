e4147ac87d4ef687df1e8e834ae0215e0eca1296
AndrewParker770 <2389622P@student.gla.ac.uk>
2021-11-11 10:11:53 +0000

    Added Bing Image Search Functionality

--- django_videography_project/videography/forms.py
+++ django_videography_project/videography/forms.py
@@ -6,7 +6,7 @@ class LinkForm(forms.Form):
     youtube_link = forms.CharField(label='YouTube Link:', max_length=100, widget=forms.TextInput(attrs={'rows':1, 'class': 'form-control'}))
     artist_name = forms.CharField(label='Artist Name:', max_length=100, widget=forms.TextInput(attrs={'rows':1, 'class': 'form-control'}))
     song_name = forms.CharField(label='Song Name:', max_length=100, widget=forms.TextInput(attrs={'rows':1, 'class': 'form-control'}))
-
+    search_term = forms.CharField(label='Image Search Term:', max_length=100, widget=forms.TextInput(attrs={'rows':1, 'class': 'form-control'}))
 
 class FeedbackForm(forms.Form):
     first_name = forms.CharField(label='First Name:', max_length=100, widget=forms.TextInput(attrs={'rows':1, 'class': 'form-control'}))
--- django_videography_project/videography/views.py
+++ django_videography_project/videography/views.py
@@ -11,6 +11,7 @@ from .forms import FeedbackForm
 
 import moviepy.editor as mpy
 import gizeh
+from bing_image_downloader import downloader
 
 def make_frame(t):
     surface = gizeh.Surface(128,128) # width, height
@@ -28,12 +29,13 @@ def index(request):
             youtubeID = getID(form.cleaned_data['youtube_link'])
             text_result = extractLyrics(form.cleaned_data['artist_name'], form.cleaned_data['song_name'], youtubeID)
 
-
+            downloader.download(form.cleaned_data['search_term'], limit=5,  output_dir='videography/static/imgs/', adult_filter_off=False, force_replace=False, timeout=60, verbose=True)
+            
             songLength = getSongLength(youtubeID)
             audio = mpy.AudioFileClip("Source/AudioFiles/%s.wav"%(youtubeID))
             clip = mpy.VideoClip(make_frame, duration=songLength)
             clip.audio = audio
-            clip.set_duration(songLength).write_videofile("videography/static/%s.mp4"%(youtubeID), fps=24)
+            clip.set_duration(songLength).write_videofile("videography/static/videos/%s.mp4"%(youtubeID), fps=24)
 
             form = LinkForm()
             context_dict = {'currentpage': 'Index', 'form': form }
@@ -49,8 +51,8 @@ def about(request):
     return render(request, 'videography/about.html', context=context_dict)
 
 def video(request, pk):
-    file_path = str(pk) + '.mp4'
-    context_dict = {'currentpage': 'Video', 'file_path': file_path}
+    video_file_path = 'videos/' + str(pk) + '.mp4'
+    context_dict = {'currentpage': 'Video', 'video_file_path': video_file_path}
     return render(request, 'videography/video.html', context=context_dict)
 
 def collection(request):
